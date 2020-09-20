505a1adba03aa13f0a15d327ed74303eedf2bfb2
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2020-09-20 16:34:23 +0300

    data stored in struct and VM working with my program

--- filler.c
+++ filler.c
@@ -12,42 +12,29 @@ static int get_piece(t_piece *piece, char *pieceline, int fd)
 		return (0);
 	piece->y = ft_atoi(res[0]);
 	piece->x = ft_atoi(res[1]);
-	// printf("y: %d\n", piece->y);
-	// printf("x: %d\n", piece->x);
+	printf("y: %d\n", piece->y);
+	printf("x: %d\n", piece->x);
 	free(res[0]);
 	free(res[1]);
 	free(res);
 	if (!(piece->piece = (char**)malloc(sizeof(char*) * piece->y + 1)))
 		return (0);
-	ft_putstr_fd("piece y:", 2);
-	ft_putnbr_fd(piece->y, 2);
-	ft_putchar_fd('\n', 2);
-	ft_putstr_fd("piece x:", 2);
-	ft_putnbr_fd(piece->x, 2);
-	ft_putchar_fd('\n', 2);
-	ft_putstr_fd(line, 2);
-	ft_putnbr_fd(fd, 2);
-	/*
-	while (get_next_line(fd, &line) == 1)
+	while (nb < piece->y && get_next_line(fd, &line) == 1)
 	{
-		ft_putstr_fd("IN GNL AT GETPIECE\n", 2);
 		if (line)
 		{
 			piece->piece[nb] = ft_strdup(line);
 			nb++;
-			ft_putstr_fd("IN LINE AT GETPIECE\n", 2);
 		}
-	//	ft_strdel(&line);
 		if (nb == piece->y)
 			break ;
 	}
 	piece->piece[nb] = NULL;
-	*/
-	// while (*piece->piece != NULL)
-	// {
-	// 	printf("piece: %s\n", *piece->piece);
-	// 	piece->piece++;
-	// }
+	while (*piece->piece != NULL)
+	{
+		printf("piece: %s\n", *piece->piece);
+		piece->piece++;
+	}
 	return (1);
 }
 
@@ -64,23 +51,20 @@ static char	*get_map(t_map *map, char *mapline, int fd)
 		return (0);
 	map->y = ft_atoi(res[0]);
 	map->x = ft_atoi(res[1]);
-	// printf("y: %d\n", map->y);
-	// printf("x: %d\n", map->x);
+	printf("y: %d\n", map->y);
+	printf("x: %d\n", map->x);
 	free(res[0]);
 	free(res[1]);
 	free(res);
 	if (!(map->map = (char**)malloc(sizeof(char*) * map->x + 1)))
 		return (0);
 	get_next_line(fd, &line);
-	// ft_strdel(&line);
 	while (ft_strstr(line, "Piece") == NULL && get_next_line(fd, &line) == 1)
 	{
-		ft_putstr_fd("IN GNL AT GETMAP\n", 2);
 		if (line)
 		{	
 			if (nb == map->y)
 				break ;
-			ft_putstr_fd("IN LINE AT GETMAP\n", 2);
 			map->map[nb] = ft_strdup(&line[4]);
 			if (ft_strstr(line, "O"))
 			{
@@ -89,7 +73,6 @@ static char	*get_map(t_map *map, char *mapline, int fd)
 					y++;
 				map->p1_x = y - 4;
 				y = 0;
-				
 			}
 			if (ft_strstr(line, "X"))
 			{
@@ -101,18 +84,17 @@ static char	*get_map(t_map *map, char *mapline, int fd)
 			}
 			nb++;	
 		}
-		//ft_strdel(&line);
 	}
 	map->map[nb] = NULL;
-	// while (*map->map != NULL)
-	// {
-	// 	printf("map: %s\n", *map->map);
-	// 	map->map++;
-	// }
-	// printf("p1_y: %d\n", map->p1_y);
-	// printf("p1_x: %d\n", map->p1_x);
-	// printf("p2_y: %d\n", map->p2_y);
-	// printf("p2_x: %d\n", map->p2_x);
+	while (*map->map != NULL)
+	{
+		printf("map: %s\n", *map->map);
+		map->map++;
+	}
+	printf("p1_y: %d\n", map->p1_y);
+	printf("p1_x: %d\n", map->p1_x);
+	printf("p2_y: %d\n", map->p2_y);
+	printf("p2_x: %d\n", map->p2_x);
 	return (line);
 }
 
@@ -124,7 +106,7 @@ static int get_player(t_players *players , char *line)
 		players->playernum = 1;
 	else
 		players->playernum = 2;	
-	// printf("playernum: %d\n", players->playernum);
+	printf("playernum: %d\n", players->playernum);
 	return(1);
 }
 
@@ -149,34 +131,34 @@ int		main(int argc, char **argv) // only for debugging with a file
 		return (0);
 	//if (!(coordinates = (t_coordinates *)malloc(sizeof(t_coordinates))))
 	//	return (0);
-	while (get_next_line(fd, &line) == 1)
+	while (get_next_line(fd2, &line) == 1)
 	{
-		ft_putstr_fd("line before statements:", 2);
-		ft_putstr_fd(line, 2);
-		ft_putchar_fd('\n', 2);
+		// ft_putstr_fd("line before statements:", 2);
+		// ft_putstr_fd(line, 2);
+		// ft_putchar_fd('\n', 2);
 		if (line && ft_strstr(line, "$$$"))
-			get_player(players, line);
-		if (line && ft_strstr(line, "Plateau"))
-			line = get_map(map, line, fd);
-		if (line && ft_strstr(line, "Piece"))
 		{
-			if (get_piece(piece, line, fd) == 1)
+			if (get_player(players, line) == 0)
 				break ;
 		}
-		ft_putstr_fd("line after statements:", 2);
-		ft_putstr_fd(line, 2);
-		ft_putchar_fd('\n', 2);
+		if (line && ft_strstr(line, "Plateau"))
+			line = get_map(map, line, fd2);
+		if (line && ft_strstr(line, "Piece"))
+		{
+			if (get_piece(piece, line, fd2) == 1)
+				break ;
+		}
+		// ft_putstr_fd("line after statements:", 2);
+		// ft_putstr_fd(line, 2);
+		// ft_putchar_fd('\n', 2);
 	}
 	ft_putnbr(map->p1_y);
 	ft_putchar(' ');
 	ft_putnbr(map->p1_x);
 	ft_putchar('\n');
-	ft_putnbr_fd(map->p1_y, 2);
-	ft_putnbr_fd(map->p1_x, 2);
 	//coordinates = heatmap(map, piece, players);
 	free(map);
 	free(piece);
 	free(players);
-	//close(fd); // only for debugging via file
 	return(0);
 }
