7059816c1e83526cb5b72aa2a58c39e3baa46d12
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2020-10-16 10:53:08 +0300

    convert_map now norm

--- construct_map.c
+++ construct_map.c
@@ -6,106 +6,109 @@
 /*   By: Melissa <Melissa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 12:33:23 by malasalm          #+#    #+#             */
-/*   Updated: 2020/10/15 20:06:42 by Melissa          ###   ########.fr       */
+/*   Updated: 2020/10/16 10:51:23 by Melissa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-int ft_abs(int n)
+static int		ft_abs(int n)
 {
-    if (n < 0)
+	if (n < 0)
 		n = n * -1;
-    return (n);
+	return (n);
 }
 
-int fill_heatmap(t_filler *data, int row, int column)
+static int		fill_heatmap(t_filler *data, int row, int column)
 {
-    int y;
-    int x;
-    int distance;
-    int min;
+	int y;
+	int x;
+	int distance;
+	int min;
 
-    y = 0;
-    min = 0;
-    while (y < data->map_y)
-    {   
-        x = 0;
-        while (x < data->map_x)
-        {
-            if (data->heatmap[y][x] == -2)
-            {
-                distance = ft_abs(x - column);
-                distance += ft_abs(y - row);
-                if (distance < min || min == 0)
-                    min = distance;
-            }
-            x++;
-        }
-        y++;
-    }
-    return(min);
+	y = 0;
+	min = 0;
+	while (y < data->map_y)
+	{
+		x = 0;
+		while (x < data->map_x)
+		{
+			if (data->heatmap[y][x] == -2)
+			{
+				distance = ft_abs(x - column);
+				distance += ft_abs(y - row);
+				if (distance < min || min == 0)
+					min = distance;
+			}
+			x++;
+		}
+		y++;
+	}
+	return (min);
 }
 
-void construct_heatmap(t_filler *data)
+static void		construct_heatmap(t_filler *data)
 {
-    int y;
-    int x;
+	int y;
+	int x;
 
-    y = 0;
-    while (y < data->map_y)
-    {    
-        x = 0;
-        while (x < data->map_x)
-        {
-            if (data->heatmap[y][x] == 0)
-                data->heatmap[y][x] = fill_heatmap(data, y, x);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	while (y < data->map_y)
+	{
+		x = 0;
+		while (x < data->map_x)
+		{
+			if (data->heatmap[y][x] == 0)
+				data->heatmap[y][x] = fill_heatmap(data, y, x);
+			x++;
+		}
+		y++;
+	}
 }
 
-int construct_map(t_filler *data, int p)
+static void		convert_data(t_filler *data, int p, int y, int x)
 {
-    int y;
-    int x;
+	while (y < data->map_y)
+	{
+		x = 0;
+		while (x < data->map_x)
+		{
+			if (data->map[y][x] == '.')
+				data->heatmap[y][x] = 0;
+			if (data->map[y][x] == 'o' || data->map[y][x] == 'O')
+			{
+				if (p == 1)
+					data->heatmap[y][x] = -1;
+				else
+					data->heatmap[y][x] = -2;
+			}
+			if (data->map[y][x] == 'x' || data->map[y][x] == 'X')
+			{
+				if (p == 2)
+					data->heatmap[y][x] = -1;
+				else
+					data->heatmap[y][x] = -2;
+			}
+			x++;
+		}
+		y++;
+	}
+}
 
-    y = 0;
-    if (!(data->heatmap = (int**)malloc(sizeof(int*) * data->map_y)))
+int				construct_map(t_filler *data, int p)
+{
+	int y;
+
+	y = 0;
+	if (!(data->heatmap = (int**)malloc(sizeof(int*) * data->map_y)))
 		return (1);
-    while (y < data->map_y)
-    {
-        if(!(data->heatmap[y] = (int*)malloc(sizeof(int) * data->map_x)))
-            return(1);
-        y++;
-    }
-    y = 0;
-    while (y < data->map_y)
-    {
-        x = 0;
-        while (x < data->map_x)
-        {
-            if (data->map[y][x] == '.')
-                data->heatmap[y][x] = 0;
-            if (data->map[y][x] == 'o' || data->map[y][x] == 'O')
-            {
-                if(p == 1)
-                    data->heatmap[y][x] = -1;
-                else
-                    data->heatmap[y][x] = -2;
-            }
-            if (data->map[y][x] == 'x' || data->map[y][x] == 'X')
-            {
-                if(p == 2)
-                    data->heatmap[y][x] = -1;
-                else
-                    data->heatmap[y][x] = -2;
-            }
-            x++;
-        }
-        y++;
-    }
-    construct_heatmap(data);
-    return (0);
+	while (y < data->map_y)
+	{
+		if (!(data->heatmap[y] = (int*)malloc(sizeof(int) * data->map_x)))
+			return (1);
+		y++;
+	}
+	convert_data(data, p, 0, 0);
+	construct_heatmap(data);
+	return (0);
 }
